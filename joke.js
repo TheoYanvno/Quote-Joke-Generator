8359693caa0ee651c340d64d35ad8ec15d9a3f0c
TheoYanvno <TheoYanvno@gmail.com>
2023-02-11 01:34:42 +0000

    inital push

--- joke.js
+++ joke.js
@@ -1,1 +1,32 @@
+const jokeContainer = document.getElementById('joke-container');
+const jokeText = document.getElementById('joke');
+const newJokeBtn = document.getElementById('new-joke');
 
+
+let apiJokes = [];
+
+
+
+function newJoke(){
+    const joke= apiJokes[Math.floor(Math.random() * apiJokes.length)];
+    return JokeText.textContent = joke.text;
+}
+    
+
+
+async function getJokes(){
+    const apiUrl=  'http://icanhazdadjoke.com';
+    try {
+        const response = await fetch(apiUrl);
+        apiJokes= await response.json();
+        newJoke();
+    }catch(error){
+
+    }
+}
+
+
+newJokeBtn.addEventListener('click', newJoke);
+
+
+getJokes();
